1f67e91bff7b740e4e0f74c288d9a2c0963b3637
Sandeep Prasad <sandeepprasad0707@gmail.com>
2022-10-12 23:31:55 +0530

    add file

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.assignement.my_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
